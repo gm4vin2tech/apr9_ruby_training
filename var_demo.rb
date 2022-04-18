4587f98d92b4a6f76a1f6dcc1efcb1079b23e177
gm4vin2tech <gm4vin2tech@gmail.com>
2022-04-18 10:58:43 +0530

    var demo

---
+++ var_demo.rb
@@ -0,0 +1,11 @@
+#variable demo program
+a = 10
+b = 10.23
+c = "fosvsva"
+# a , b , c = 10, 10.23, "safsadf"
+
+# a is 10 and its class is integer
+puts "a is #{a} and its class is #{a.class}"
+puts "b is #{b} and its class is #{b.class}"
+puts "c is #{c} and its class is #{c.class}"
+
